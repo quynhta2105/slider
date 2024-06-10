8a1e16e5f24e46d0bed57f7e4450d36c033fbf7a
Quynh <taq2105@gmail.com>
2024-06-10 17:34:10 +0700

    update parallax

---
+++ parallax/index.js
@@ -0,0 +1,93 @@
+let xPos = 0;
+
+const cardImages = document.querySelectorAll(".ring .img");
+
+function fetchDogApi() {
+  fetch("https://dog.ceo/api/breeds/image/random/10")
+    .then((response) => response.json())
+    .then((data) => {
+      const img = data.message;
+
+      img.forEach((image, index) => {
+        cardImages[index].style.backgroundImage = `url(${image})`;
+      });
+
+      gsap.timeline().call(() => init());
+    })
+    .catch((error) => console.error("Error fetching images:", error));
+}
+
+gsap
+  .timeline().add(fetchDogApi)
+  .set(".ring", { rotationY: 180, cursor: "grab" })
+  .set(".img", {
+    // apply transform rotations to each image
+    rotateY: (i) => i * -36,
+    transformOrigin: "50% 50% 500px",
+    z: -500,
+    backgroundImage: (i) =>
+      "url(https://picsum.photos/id/" + (i + 32) + "/600/400/)",
+    backgroundPosition: (i) => getBgPos(i),
+    backfaceVisibility: "hidden",
+  })
+  .from(".img", {
+    duration: 1.5,
+    y: 200,
+    opacity: 0,
+    stagger: 0.1,
+    ease: "expo",
+  })
+  .add(() => {
+    $(".img").on("mouseenter", (e) => {
+      let current = e.currentTarget;
+      gsap.to(".img", {
+        opacity: (i, t) => (t == current ? 1 : 0.5),
+        ease: "power3",
+      });
+    });
+    $(".img").on("mouseleave", (e) => {
+      gsap.to(".img", { opacity: 1, ease: "power2.inOut" });
+    });
+  }, "-=0.5");
+
+$(window).on("mousedown touchstart", dragStart);
+$(window).on("mouseup touchend", dragEnd);
+
+function dragStart(e) {
+  if (e.touches) e.clientX = e.touches[0].clientX;
+  xPos = Math.round(e.clientX);
+  gsap.set(".ring", { cursor: "grabbing" });
+  $(window).on("mousemove touchmove", drag);
+}
+
+function drag(e) {
+  if (e.touches) e.clientX = e.touches[0].clientX;
+
+  gsap.to(".ring", {
+    rotationY: "-=" + ((Math.round(e.clientX) - xPos) % 360),
+    onUpdate: () => {
+      gsap.set(".img", { backgroundPosition: (i) => getBgPos(i) });
+    },
+  });
+
+  xPos = Math.round(e.clientX);
+}
+
+function dragEnd(e) {
+  $(window).off("mousemove touchmove", drag);
+  gsap.set(".ring", { cursor: "grab" });
+}
+
+function getBgPos(i) {
+  return (
+    100 -
+    (gsap.utils.wrap(
+      0,
+      360,
+      gsap.getProperty(".ring", "rotationY") - 180 - i * 36
+    ) /
+      360) *
+      500 +
+    "px 0px"
+  );
+}
